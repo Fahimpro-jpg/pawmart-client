0634468773bb279734de62982e68db65ef63427f
Fahimpro-jpg <fahadalamfahim111844@gmail.com>
2025-11-09 21:11:15 +0600

    the navbar is finished

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
-
+import { Link, NavLink } from 'react-router';
+import logo from '../../assets/7a1c0287-194d-49f0-bf94-2fab8ac65b41.png'
 const Navbar = () => {
+   const links = <>
+    <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/petAndSupplies">Pet & Supplies</NavLink></li>
+      <li><NavLink to="/addListings">Add Listings</NavLink></li>
+      <li><NavLink to="/myListings">My Listings</NavLink></li>
+      <li><NavLink to="/myOrders">My Orders</NavLink></li>
+    </>
     return (
         <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
@@ -11,36 +19,24 @@ const Navbar = () => {
       <ul
         tabIndex="-1"
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        {links}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <div className='flex gap-4'>
+        <img className='w-[30px] h-[30px] rounded-xl ' src={logo} alt="" />
+        <p className="text-2xl font-bold"><span className='text-sky-400'>Paw</span><span className='text-amber-400'>Mart</span></p>
+    </div>
+    
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {links}
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+      <Link className='btn border-sky-200 bg-sky-400' to='/login'>Login</Link>
+      <p className='ml-2 mr-2 text-2xl text-blue-900 font-bold'>OR</p>
+      <Link className='btn' to='/register'>Register</Link>
   </div>
 </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,11 @@ import { RouterProvider } from "react-router/dom";
 import RootLayout from './Layouts/RootLayout.jsx';
 import Home from './components/Home/Home.jsx';
 import AllProducts from './components/AllProducts/AllProducts.jsx';
+import Login from './pages/Login/Login.jsx';
+import Register from './pages/Register/Register.jsx';
+import AddListings from './components/AddListings/AddListings.jsx';
+import MyListings from './components/MyListings/MyListings.jsx';
+import MyOrders from './components/MyOrders/MyOrders.jsx';
 const router = createBrowserRouter([
   {
     path:'/',
@@ -19,6 +24,26 @@ const router = createBrowserRouter([
       {
         path:'/petAndSupplies',
         Component: AllProducts
+      },
+      {
+        path:'/login',
+        Component:Login
+      },
+      {
+        path:'/register',
+        Component:Register
+      },
+      {
+        path:'/addListings',
+        element:<AddListings></AddListings>
+      },
+      {
+        path:'/myListings',
+        element:<MyListings></MyListings>
+      },
+      {
+        path:'/myOrders',
+        element:<MyOrders></MyOrders>
       }
     ]
   }
