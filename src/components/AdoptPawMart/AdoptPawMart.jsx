ca8e9ed80208778282bbcd2047a48827c69462e8
Fahimpro-jpg <fahadalamfahim111844@gmail.com>
2025-11-10 21:13:23 +0600

    sending data issue was fixed

--- src/components/AdoptPawMart/AdoptPawMart.jsx
+++ src/components/AdoptPawMart/AdoptPawMart.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import adoptedPet from "../../assets/adpotingpet.jpeg";
 
 const AdoptPawMart = () => {
     return (
         <div
-            className="w-[1200px] mx-auto bg-cover bg-center"
-            style={{ backgroundImage: `url(${adoptedPet})` }}
+           
         >
             
         </div>
